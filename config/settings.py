c2347a76bc800e115ad3f2ebb9556d6e7d4d8630
LucasKim4321 <xowls0131@naver.com>
2025-04-06 17:58:58 +0900

    .

--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,13 @@ SECRET_KEY = SECRET['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
+
+# DEBUG = False
+# ALLOWED_HOSTS = [
+#     '127.0.0.1',
+#     'localhost',
+# ]
 
 
 # Application definition
